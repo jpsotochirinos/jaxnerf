4ea395562ea04fa7ca9c2e5a4f02743a9994457c
Jean Soto <jpsotochirinos@gmail.com>
2021-11-17 00:19:09 -0500

    new req

--- start_install.sh
+++ start_install.sh
@@ -2,13 +2,7 @@ pip install skia-python
 
 git clone https://github.com/LordCocoro/jaxnerf.git
 
-conda create --name jaxnerf python=3.7
-
-echo "source activate jaxnerf" > ~/.bashrc
-
-PATH /opt/conda/envs/env/bin:$PATH
-
-conda install pip; pip install --upgrade pip setuptools wheel
+pip install --upgrade pip setuptools wheel
 
 pip install -r jaxnerf/requirements.txt
 
