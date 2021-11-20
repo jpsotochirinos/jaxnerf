7a01a1957fb198581e71bfcbd7e62cc7a4712723
Jean Soto <jpsotochirinos@gmail.com>
2021-11-19 19:08:53 -0500

    init tpu

---
+++ init_tpu.sh
@@ -0,0 +1,6 @@
+arrIN=(${KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS//:/})
+arrIN=(${arrIN//grpc\/\//})
+arrIN=(${arrIN//8470/})
+TPU_IP=http://${arrIN[0]}:8475/requestversion/tpu_driver_nightly
+
+curl "${TPU_IP}"
