ccb822d87bc18c56757cb12428de006f509b4dbb
Jean Soto <jpsotochirinos@gmail.com>
2021-11-20 10:09:07 -0500

    test

--- __init__.py
+++ __init__.py
@@ -13,3 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+import requests
+from jax import config
+
+path = os.getenv('KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS')
+path = path.split(':')
+url = 'http://'+path[1][2:]+':8475/requestversion/tpu_driver_nightly'
+reqq = requests.post(url)
+print(reqq)
+TPU_DRIVER_MODE = 1
+config.FLAGS.jax_xla_backend = "tpu_driver"
+config.FLAGS.jax_backend_target = os.getenv('KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS')
--- train.py
+++ train.py
@@ -19,15 +19,6 @@ import os
 import requests
 from jax import config
 
-path = os.getenv('KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS')
-path = path.split(':')
-url = 'http://'+path[1][2:]+':8475/requestversion/tpu_driver_nightly'
-reqq = requests.post(url)
-print(reqq)
-TPU_DRIVER_MODE = 1
-config.FLAGS.jax_xla_backend = "tpu_driver"
-config.FLAGS.jax_backend_target = os.getenv('KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS')
-
 import functools
 import gc
 import time
@@ -131,8 +122,8 @@ def main(unused_argv):
   rng = random.PRNGKey(20200823)
   # Shift the numpy random seed by host_id() to shuffle data loaded by different
   # hosts.
-  np.random.seed(20201473 + jax.process_index())
-  print(jax.devices())
+  np.random.seed(20201473 + jax.host_id())
+
   if FLAGS.config is not None:
     utils.update_flags(FLAGS)
   if FLAGS.batch_size % jax.device_count() != 0:
