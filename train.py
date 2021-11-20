320dff5dccbe27506b98b039c98a946de3d20fd8
Jean Soto <jpsotochirinos@gmail.com>
2021-11-19 23:04:23 -0500

    fix

--- train.py
+++ train.py
@@ -19,15 +19,6 @@ import os
 import requests
 from jax import config
 
-path = os.getenv('KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS')
-path = path.split(':')
-url = 'http://'+path[1][2:]+':8475/requestversion/tpu_driver_nightly'
-reqq = requests.post(url)
-TPU_DRIVER_MODE = 1
-config.FLAGS.jax_xla_backend = "tpu_driver"
-config.FLAGS.jax_backend_target = os.getenv('KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS')
-
-
 import functools
 import gc
 import time
@@ -46,18 +37,24 @@ from jaxnerf.nerf import datasets
 from jaxnerf.nerf import models
 from jaxnerf.nerf import utils
 
-
+TPU_DRIVER_MODE = 0
 
 FLAGS = flags.FLAGS
 
 utils.define_flags()
 config.parse_flags_with_absl()
-
-def train_step(model, rng, state, batch, lr):
+def init_tpu():
   global TPU_DRIVER_MODE
+  path = os.getenv('KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS')
+  path = path.split(':')
+  url = 'http://'+path[1][2:]+':8475/requestversion/tpu_driver_nightly'
+  reqq = requests.post(url)
   TPU_DRIVER_MODE = 1
   config.FLAGS.jax_xla_backend = "tpu_driver"
   config.FLAGS.jax_backend_target = os.getenv('KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS')
+
+def train_step(model, rng, state, batch, lr):
+  init_tpu()
   """One optimization step.
 
   Args:
@@ -131,11 +128,7 @@ def train_step(model, rng, state, batch, lr):
 
 
 def run_train(flg):
-  global TPU_DRIVER_MODE
-  TPU_DRIVER_MODE = 1
-  config.FLAGS.jax_xla_backend = "tpu_driver"
-  config.FLAGS.jax_backend_target = os.getenv('KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS')
-
+  init_tpu()
   rng = random.PRNGKey(20200823)
   # Shift the numpy random seed by host_id() to shuffle data loaded by different
   # hosts.
