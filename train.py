b9810ed63e20026e725506548fdaba0fb94b01f6
Jean Soto <jpsotochirinos@gmail.com>
2021-11-22 14:56:33 -0500

    test

--- train.py
+++ train.py
@@ -19,15 +19,13 @@ import os
 import requests
 from jax import config
 
-path = os.getenv('KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS')
-path = path.split(':')
-url = 'http://'+path[1][2:]+':8475/requestversion/tpu_driver_nightly'
-reqq = requests.post(url)
-print(reqq)
-TPU_DRIVER_MODE = 1
-
-config.FLAGS.jax_backend_target = os.getenv('KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS')
-
+# path = os.getenv('KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS')
+# path = path.split(':')
+# url = 'http://'+path[1][2:]+':8475/requestversion/tpu_driver_nightly'
+# reqq = requests.post(url)
+# print(reqq)
+import jax.tools.colab_tpu
+jax.tools.colab_tpu.setup_tpu()
 
 import functools
 import gc
