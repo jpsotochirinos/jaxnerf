533d26777af27274e4e9afab56923511f267beba
Jean Pierre Soto Chirinos <LordCocoro@users.noreply.github.com>
2021-11-25 07:59:57 -0500

    Delete start_install.sh

--- start_install.sh
+++
@@ -1,9 +0,0 @@
-pip install skia-python
-
-git clone https://github.com/LordCocoro/jaxnerf.git
-
-pip install --upgrade pip setuptools wheel
-
-pip install -r jaxnerf/requirements.txt
-
-pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
